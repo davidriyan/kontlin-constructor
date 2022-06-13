0107bf1862cf6f31c796c09a68e2cac7832a73a2
david riyan kurniawan <103322177+davidriyan@users.noreply.github.com>
2022-06-14 00:26:11 +0700

    Add files via upload

---
+++ src/main/kotlin/App/Constructor.kt
@@ -0,0 +1,15 @@
+package App
+
+import Data.Car
+fun main(){
+    val toyota = Car ("honda jazz")
+    val honda = Car ("avanza")
+    println(toyota.Brand)
+    println(toyota.Year)
+    println(honda.Brand)
+    println(honda.Year)
+
+}
+
+
+
---
+++ src/main/kotlin/Data/Car.kt
@@ -0,0 +1,6 @@
+package Data
+
+class Car(ParamBrand: String , ParamYear: Int =2022) {
+    var Brand:String = ParamBrand
+    var Year:Int = ParamYear
+}
